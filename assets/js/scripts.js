6c248aae9d5f4f54122561446134ced631a23660
AlberthRKO <orlando.alb77@gmail.com>
2020-11-25 23:59:01 -0400

    registros lectura , precio  y descuento

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,20 @@
-(function(window, undefined) {
+(function (window, undefined) {
   'use strict';
 
-  /*
-  NOTE:
-  ------
-  PLACE HERE YOUR OWN JAVASCRIPT CODE IF NEEDED
-  WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR JAVASCRIPT CODE PLEASE CONSIDER WRITING YOUR SCRIPT HERE.  */
+  // muestra del file-upload
+  $('.tipoLectura').on('change', function () {
+    if (this.value === '1') {
+      $('.file-upload').addClass('d-block');
+    } else {
+      $('.file-upload').removeClass('d-block');
+    }
+    if (this.value === '2') {
+      $('.registroConsumo').prop('disabled', true);
+      $('.registroConsumo').val('30');
+    } else {
+      $('.registroConsumo').prop('disabled', false);
+      $('.registroConsumo').val('');
+    }
+  });
 
 })(window);
